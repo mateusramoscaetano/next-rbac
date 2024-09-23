084b6a5daf0e64f3b2da1f8ee4cd91008ae5ebc3
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-23 16:19:53 -0300

    route to get profile

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,47 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { auth } from '@/http/middlewares/auth'
+
+export async function getProfile(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/profile',
+      {
+        schema: {
+          tags: ['auth'],
+          summary: ' Get authenticate user profile',
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              user: z.object({
+                id: z.string().uuid(),
+                name: z.string().nullable(),
+                email: z.string().email(),
+                avatarUrl: z.string().url().nullable(),
+              }),
+            }),
+            400: z.object({ message: z.string() }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          where: { id: userId },
+          select: { id: true, name: true, email: true, avatarUrl: true },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User profile not found.')
+        }
+
+        return reply.status(200).send({ user })
+      },
+    )
+}
