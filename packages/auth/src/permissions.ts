7550a1bd59f55b09e0bb3ad15edd870c15850874
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-23 11:50:08 -0300

    Add all permissions

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -6,15 +6,24 @@ import { Role } from './roles'
 
 type PermissionsByRole = (
   user: User,
+
   builder: AbilityBuilder<AppAbility>,
 ) => void
 
 export const permissions: Record<Role, PermissionsByRole> = {
-  ADMIN(_, { can }) {
+  ADMIN(user, { can, cannot }) {
     can('manage', 'all')
+    cannot(['transfer_ownership', 'update'], 'Organization')
+    can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: { $eq: user.id },
+    })
   },
-  MEMBER(_, { can }) {
-    can('invite', 'User')
+  MEMBER(user, { can }) {
+    can('get', 'User')
+    can(['create', 'get'], 'Project')
+    can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } })
   },
-  BILLING() {},
+  BILLING(_, { can }) {
+    can('manage', 'Billing')
+  },
 }
