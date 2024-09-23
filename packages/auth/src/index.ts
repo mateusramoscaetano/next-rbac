54cde0fa04235c0c425231ef7ed3210357c1a814
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-23 16:23:42 -0300

    Add env package and update auth package permissions

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -14,6 +14,11 @@ import { organizationSubject } from './subjects/organization'
 import { projectSubject } from './subjects/project'
 import { userSubject } from './subjects/user'
 
+export * from './models/organization'
+export * from './models/project'
+export * from './models/user'
+export * from './roles'
+
 const appAbilitiesSchema = z.union([
   projectSubject,
   userSubject,
@@ -32,12 +37,19 @@ export function defineAbilityFor(user: User) {
   const builder = new AbilityBuilder(createAppAbility)
 
   if (typeof permissions[user.role] !== 'function') {
-    throw new Error('Error de permissão')
+    throw new Error(`Permissions for role ${user.role} not found.`)
   }
 
   permissions[user.role](user, builder)
 
-  const ability = builder.build()
+  const ability = builder.build({
+    detectSubjectType(subject) {
+      return subject.__typename
+    },
+  })
+
+  ability.can = ability.can.bind(ability)
+  ability.cannot = ability.cannot.bind(ability)
 
   return ability
 }
--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -6,13 +6,13 @@ import { Role } from './roles'
 
 type PermissionsByRole = (
   user: User,
-
   builder: AbilityBuilder<AppAbility>,
 ) => void
 
 export const permissions: Record<Role, PermissionsByRole> = {
   ADMIN(user, { can, cannot }) {
     can('manage', 'all')
+
     cannot(['transfer_ownership', 'update'], 'Organization')
     can(['transfer_ownership', 'update'], 'Organization', {
       ownerId: { $eq: user.id },
