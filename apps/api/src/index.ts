ca83aeb6eda68edd6c1d60f1ae6dcd434abd9e56
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-23 16:16:24 -0300

    Remove index.ts

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER' })
-
-const canInvite = ability.can('invite', 'User')
-const canDelete = ability.can('delete', 'User')
-
-console.log(canInvite)
-console.log(canDelete)
