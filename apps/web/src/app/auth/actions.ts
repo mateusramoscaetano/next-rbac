784768c0c2e5b11a844f3c89c7a6103e85cc50c5
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-26 08:38:42 -0300

    Add actions auth global

---
+++ apps/web/src/app/auth/actions.ts
@@ -0,0 +1,12 @@
+import { redirect } from 'next/navigation'
+
+export async function signInWithGithub() {
+  const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
+  githubSignInURL.searchParams.set('client_id', 'Ov23ctu2HH5PrMSiXh7Q')
+  githubSignInURL.searchParams.set(
+    'redirect_uri',
+    'http://localhost:3000/api/auth/callback',
+  )
+  githubSignInURL.searchParams.set('scope', 'user')
+  redirect(githubSignInURL.toString())
+}
