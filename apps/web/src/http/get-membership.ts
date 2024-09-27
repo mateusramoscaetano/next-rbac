fe0906bbc79646fe180df6b292315f4fe5a5440f
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-27 09:46:46 -0300

    Add get permissions for the user logged

---
+++ apps/web/src/http/get-membership.ts
@@ -0,0 +1,17 @@
+import type { Role } from '@saas/auth'
+
+import { api } from './api-client'
+interface GetOrganizationsResponse {
+  membership: {
+    id: string
+    role: Role
+    userId: string
+    organizationId: string
+  }
+}
+export async function getMembership(org: string) {
+  const result = await api
+    .get(`organizations/${org}/membership`)
+    .json<GetOrganizationsResponse>()
+  return result
+}
