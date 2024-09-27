3cbbe4f36e695cb4ef87f58d6f0a5d91ff14657a
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-27 08:28:17 -0300

    Add organization switcher

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,7 +1,9 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import squirrel from '@/assets/logo.svg'
 
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
@@ -10,7 +12,9 @@ export function Header() {
       <div className='mx-auto flex max-w-[1200px] items-center justify-between'>
         <div className='flex items-center gap-3 font-medium'>
           <Image src={squirrel} className='size-6 dark:invert' alt='logo' />
-          squirrel saas
+
+          <Slash className='size-4 -rotate-[24deg] text-border' />
+          <OrganizationSwitcher />
         </div>
         <div className='flex items-center gap-4'>
           <ProfileButton />
